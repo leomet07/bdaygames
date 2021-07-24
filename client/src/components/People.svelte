3469acab345493c0bba19614d6298ef6db3953a6
leomet07 <leomet07@gmail.com>
2021-07-24 15:31:50 -0400

    Turn meter done

--- client/src/components/People.svelte
+++ client/src/components/People.svelte
@@ -1,5 +1,52 @@
+<script>
+	import { players, turn_index } from "../stores";
+
+	async function addPlayer(e) {
+		e.preventDefault();
+
+		let input = document.getElementById("playername");
+
+		let playername = input.value;
+
+		$players = [...$players, { name: playername }];
+
+		input.value = "";
+	}
+
+	async function increment() {
+		if ($turn_index < $players.length - 1) {
+			$turn_index++;
+		} else {
+			$turn_index = 0;
+		}
+	}
+</script>
+
 <main id="people">
-	<div id="floater"><h1>People</h1></div>
+	<div id="floater">
+		<h1>People</h1>
+		<div id="playersdiv">
+			{#each $players as player, index}
+				<p>Player #{index + 1}: {player.name}</p>
+			{/each}
+			{#if $players.length > 0}
+				<p>
+					Current turn: Player #{$turn_index + 1} (AKA: {$players[
+						$turn_index
+					]["name"]})
+				</p>
+			{:else}
+				<p>Add some of the players' names</p>
+			{/if}
+
+			<button on:click={increment}>Increment</button>
+		</div>
+
+		<form on:submit={addPlayer}>
+			<input type="text" id="playername" />
+			<input value="Add Player" type="submit" />
+		</form>
+	</div>
 </main>
 
 <style>
--- client/src/stores.js
+++ client/src/stores.js
@@ -1,3 +1,5 @@
 import { writable } from "svelte/store";
 
 export const triviaquestions = writable([]);
+export const players = writable([]);
+export const turn_index = writable(0);
