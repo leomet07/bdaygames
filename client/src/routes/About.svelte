9a52fc0e73f7158fc9bcdb6649fc901fcecd0a69
leomet07 <leomet07@gmail.com>
2021-07-24 20:24:01 -0400

    About page

--- client/src/routes/About.svelte
+++ client/src/routes/About.svelte
@@ -1,1 +1,38 @@
 <h1>About</h1>
+
+<img id="logo" src="/img/bdaygames.png" alt="Bday games logo" />
+<h2>A fun website for all of your party (and birthday) celebration needs!</h2>
+
+<h2>
+	Frontend is hosted on <a target="_blank" href="https://cloud.google.com/"
+		>Google Cloud Platform</a
+	> through Firebase.
+</h2>
+
+<h2>This project was made for the July 2021 MLH Hacky Birthday hacakthon.</h2>
+
+<h2>
+	<a target="_blank" href="https://github.com/leomet07/bdaygames"
+		>Source Code</a
+	>
+	|
+	<a target="_blank" href="https://devpost.com/leomet07">Devpost</a>
+	|
+	<a target="_blank" href="https://github.com/leomet07/">leomet07's GitHub</a>
+</h2>
+
+<style>
+	#logo {
+		height: 55vh;
+	}
+
+	h2 {
+		font-size: 24px;
+	}
+
+	a {
+		font-size: 24px;
+		text-decoration: underline;
+		color: rgb(0, 0, 102);
+	}
+</style>
